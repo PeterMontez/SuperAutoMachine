eb69bf1cfb9bdc51e9797e61a051253366e173da
PeterMontez <peterson.montez9@gmail.com>
2023-05-26 09:18:55 -0300

    passives almost done

--- Actions.cs
+++ Actions.cs
@@ -1,3 +1,5 @@
+using System;
+
 public abstract class Actions
 {
     public void gainGold(int gold)
@@ -20,4 +22,32 @@ public abstract class Actions
         boneco.ChangeLife(value);
     }
 
+    public virtual void run(int position) {}
+}
+
+public abstract class GainGold : Actions 
+{
+    public override void run(int gold)
+    {
+        Game.Current.Money += gold;
+    }
+}
+
+public abstract class ChangeLife : Actions 
+{
+    //public override void run(Boneco boneco, int value)
+    public override void run(int position)
+    {
+        //boneco.ChangeLife(value);
+    }
+}
+
+public abstract class ChangeAttack : Actions 
+{
+    //public override void run(Boneco boneco, int value)
+    public override void run(int position)
+    {
+        //boneco.ChangeAttack(value);
+        team.
+    }
 }
