baf5de9b3dc5884d6276b2e8290107a61a20a720
PeterMontez <peterson.montez9@gmail.com>
2023-05-26 09:48:24 -0300

    passives almost done, again

--- Passives.cs
+++ Passives.cs
@@ -32,18 +32,68 @@ public abstract class HurtActions : PassiveList
     
 }
 
+public abstract class BeforeAttackActions : PassiveList
+{
+    
+}
+
+public abstract class EndOfTurnActions : PassiveList
+{
+    
+}
+
 public class Passive
 {
+    protected Actions effect { get; set; }
     protected int Position { get; set; }
 
-    public virtual void act() {}
+    public virtual void act() 
+    {
+        this.effect.run(Position);
+    }
 }
 
 public class Sold : Passive
 {
-    public Sold(int position)
+    public Sold(int position, Actions effect)
     {
         this.Position = position;
+        this.effect = effect;
     }
+}
 
+public class StartOfTurn : Passive
+{
+    public StartOfTurn(int position, Actions effect)
+    {
+        this.Position = position;
+        this.effect = effect;
+    }
+}
+
+public class Hurt : Passive
+{
+    public Hurt(int position, Actions effect)
+    {
+        this.Position = position;
+        this.effect = effect;
+    }
+}
+
+public class BeforeAttack : Passive
+{
+    public BeforeAttack(int position, Actions effect)
+    {
+        this.Position = position;
+        this.effect = effect;
+    }
+}
+
+public class EndOfTurn : Passive
+{
+    public EndOfTurn(int position, Actions effect)
+    {
+        this.Position = position;
+        this.effect = effect;
+    }
 }
