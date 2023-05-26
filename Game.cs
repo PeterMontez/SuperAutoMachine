497c68fb6162a232c1830a4a95509ca1dd245fdf
PeterMontez <peterson.montez9@gmail.com>
2023-05-26 08:31:11 -0300

    some stuff added

--- Game.cs
+++ Game.cs
@@ -6,9 +6,11 @@ public class WorkShop
     private static WorkShop crr = null;
     public static WorkShop Current => crr;
     public Maternity AllBonecos = new Maternity();
-
     protected List<Boneco> ShopBonecos = new List<Boneco>();
 
+
+    public Boneco GetBoneco(int index) => ShopBonecos[index]; 
+    
     public void RefreshShop()
     {
         ShopBonecos.Clear();
@@ -18,7 +20,6 @@ public class WorkShop
             ShopBonecos.Add(Maternity.New());
         }
     }
-    
     public Boneco? SellBoneco(int index) 
     {
         if(ShopBonecos[index] != null)
@@ -38,14 +39,15 @@ public class Game
     public int Money { get; set; } = 10;
     protected Team team = new Team();
     protected int round { get; set; }
-    public void Buy(int index)
+    public void Buy(int index, int index2)
     {
+        if(WorkShop.Current.GetBoneco(index) == team.GetTeam(index2) || team.GetTeam(index2) == null)
         if(this.Money >= 3 && WorkShop.Current.SellBoneco(index) != null) 
         { 
             this.Money -= 3;
             team.AddBoneco(WorkShop.Current.SellBoneco(index));
         }
-    }
 
+    }
 
 }
